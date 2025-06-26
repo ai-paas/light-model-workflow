fdde234430dd1df7cbf6fb270d968d945afa8b60
gygwak <gygwak@surromind.ai>
2025-06-26 10:25:45 +0900

    feat: sklearn onnx api 적용 (PAAS-273)
    - sklearn onnx 변환 기능
    - 관련 env sample 수정

--- app/api/v1/optimize.py
+++ app/api/v1/optimize.py
@@ -24,5 +24,7 @@ async def optimize(
         result = optimize_service.tensorrt(optimize_form)
     elif optimizer_id == 2: # 2: openvino
         result = optimize_service.openvino(optimize_form)
+    elif optimizer_id == 3: # 3: sklearn-onnx
+        result = optimize_service.sklearn_onnx(optimize_form)
     # pruning, npu, tpu, quantization, etc.
     return result
--- app/core/settings.py
+++ app/core/settings.py
@@ -50,6 +50,15 @@ class Settings(BaseSettings):
     # DETR_RESNET50
     DETR_RESNET50: str
 
+    # TENSORRT
+    TENSORRT_IMG: str
+
+    # OpenVINO
+    OPENVINO_IMG: str
+
+    # SKLEARN_ONNX
+    SKLEARN_ONNX_IMG: str
+
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
 
